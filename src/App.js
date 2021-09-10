b03e5194b3c2a29e7d74c3b7d7cd4d3bedb11962
Tianyi <inipop.tianyi@gmail.com>
2021-09-10 15:10:09 -0400

    added project list

--- src/App.js
+++ src/App.js
@@ -1,15 +1,86 @@
 import './App.css';
 import Cover1 from "../src/images/cover-3.jpg"
 import ProfilePic from "../src/images/icon-1.jpg"
+import githubIcon from "../src/images/github-icon.png"
+import mediumIcon from "../src/images/medium-icon.png"
+import doubanIcon from "../src/images/douban-icon.png"
+import instagramIcon from "../src/images/instagram-icon.png"
+import codeIcon from "../src/images/code-icon.png"
+import photoIcon from "../src/images/photo-icon.png"
 
 function App() {
   return (
     <body>
     <div className="App">
-      <img class="wallpaper" src={Cover1} alt="Wallpaper"></img>s
+      <img class="wallpaper" src={Cover1} alt="Wallpaper"></img>
       <img class="profilePic" src={ProfilePic} alt="Profile Pic"></img>
-      <p class="name"><b>Tianyi Ma</b></p>
-      <p class="intro">A senior Applied Math major at the University of Rochester, CS minor. You can usually find me solving algorithmic problems, developing softwares, writing, snapping photos and listening to podcasts and music.</p>
+      <div class="nameAndContacts">
+        <p class="name"><b>Tianyi Ma</b></p>
+        <div class="icons-box">
+            <div class="icons">
+                <a href="https://github.com/tianyi-m">
+                    <img src={githubIcon} alt="Icon"/>
+                </a>
+            </div>
+            <div class="seperator"></div>
+            <div class="icons">
+                <a href="https://www.instagram.com/sotlamb/">
+                    <img src={instagramIcon} alt="Icon"/>
+                </a>
+            </div>
+            <div class="seperator"></div>
+            <div class="icons">
+                <a href="https://medium.com/@tma8">
+                    <img src={mediumIcon} alt="Icon"/>
+                </a>
+            </div>
+            <div class="seperator"></div>
+            <div class="icons">
+                <a href="https://www.douban.com/people/243284156/">
+                    <img src={doubanIcon} alt="Icon"/>
+                </a>
+            </div>
+        </div>
+      </div>
+      <p class="intro">A senior Applied Math major at the University of Rochester, CS minor. You can usually find me solving algorithmic problems, developing softwares, writing, snapping photos and swimming. 
+      Click on the Instagram and Medium Icon to find my photos and blogs!  
+      <br/>Email: tma8@u.rochester.edu
+      </p>
+      <div class="body">
+        <div class="category">
+          <div class="categoryIcons">
+            <img class="categoryIcons" src={codeIcon} alt="Icon"/>
+          </div>
+          <div class="categoryName">
+            <p class="texts">Coding Projects</p>
+          </div>
+        </div>
+        <div class="content">
+          <p class="texts"><b>Codeforces Online Competitions and Practices (July 2021)</b>
+            <br/>• In Python, attended a few weekly algorithmic competitions and solved algorithmic problems online using number theory, dynamic programming, and greedy algorithm, and more.
+            <br/><br/><b>ML HHM, Neural Networks and MLP (April 2021)</b>
+            <br/>• Using python, built a simple Multilayer Perceptron, a two-layer Neural Network and a Hidden Markov Model from scratch. Trained the models on 5 different dateset and achieved 100%, 97%, 94%, 99$, 76% accuracy respectively. Wrote simple prediction and Viterbi prediction methods to predict the next word for a sentence for the Hidden Markov Model.
+            <br/><br/><b>Music FFT Visualizer (March 2021)</b>
+            <br/>• Using Django, MySQL and JavaScript, implemented a web browser music player that visualize the song by its amplitude using the Hanning function and by its frequency domain through applying Short-Term Fourier Transform to its signal data. 
+            <br/><br/><b>Conway’s Game of Life (March 2021)</b>
+            <br/>• In JavaScript, HTML and CSS, built a web browser game based on Conway’s Game of Life.
+            <br/><br/><b>ProjectAUX (Oct 2020)</b>
+            <br/>• In React Expo, developed an app that allow people to listen to music together with friends from anywhere.</p>
+        </div>
+      </div>
+      <div class="body">
+        <div class="category">
+          <div class="categoryIcons">
+            <img class="categoryIcons" src={photoIcon} alt="Icon"/>
+          </div>
+          <div class="categoryName">
+            <p class="texts">Photo Gallery</p>
+          </div>
+        </div>
+        <div class="content">
+          <p class="texts">TBD</p>
+        </div>
+      </div>
     </div>
     </body>
   );
